d6805fffa2045d8567cb43f1c42dc070d96260b3
arijiiiitttt🍉 <arijit123roy098@gmail.com>
2026-08-14 17:40:53 +0530

    corrected

--- index.ts
+++ index.ts
@@ -1,9 +1,8 @@
-import express from 'express';
-
-const app = express();
-
-app.get('/', (req, res) => {
-  res.send('Hi am a trial server');
+Bun.serve({
+  port: 3000, 
+  fetch(req) {
+    return new Response("Hi am a trial server");
+  },
 });
 
-app.listen(3000, () => console.log('Running on http://localhost:3000'));
+console.log("Server running on port 3000");
